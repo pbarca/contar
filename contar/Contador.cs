b0d3ab00ae8ce7c0d17d579a47a51b48a677cc6f
Paulo Coto <pbarca@hotmail.com>
2020-03-09 17:41:48 +0000

    Adicionar arquivos de projeto.

---
+++ contar/Contador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace contar
+{
+    class Contador
+    {
+        private string _path = Application.StartupPath + "\\Contador.txt";
+        public void contar(object sender)
+        {
+            Button _b = (Button)sender;
+            int valor = Convert.ToInt32(_b.Text) + 1;
+            _b.Text = valor.ToString();
+            File.WriteAllText(_path, valor.ToString());
+        }
+        public string Abrir()
+        {
+            if (File.Exists(_path)) return File.ReadAllText(_path);
+            else return "0";
+        }
+    }
+}
---
+++ contar/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace contar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = Classe.Abrir();
+        }
+        private Contador Classe = new Contador();
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Classe.contar(button1);
+        }
+    }
+}
